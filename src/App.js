c48e5524085cf3782c74104954e436056bbc2a83
yyyavuz <yusufyyavuz@outlook.com>
2020-12-08 02:53:05 +0300

    DonutChart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import BarChart from './Charts/BarChart/BarChart'
-import LineChart from './Charts/LineChartDifferentColor/LineChart'
+import BarChart from './Charts/BarChart/BarChart';
+import LineChart from './Charts/LineChartDifferentColor/LineChart';
+import DonutChart from './Charts/DonutChart/DonutChart';
  
-
 function App() {
   return (
     <div className="App">
-     <LineChart/>
+     <DonutChart/>
     </div>
   );
 }
